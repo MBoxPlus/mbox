e5db94690ca76dbc6a6d08d06e67e4ea384ccbda
Ryan Lee <liyao1021@163.com>
2021-08-31 12:32:10 +0800

    add pr comment

--- scripts/release/run.rb
+++ scripts/release/run.rb
@@ -36,9 +36,14 @@ def release(github_token, package_path, branch='main')
     release_file_path = File.join(File.dirname(package_path), "mbox-#{version}.tar.gz")
     FileUtils.cp(File.join(package_path, "release.tar.gz"), release_file_path)
     api.upload_release_asset(upload_url, release_file_path)
+    set_output("RELEASED_VERSION", version)
   else
     raise "Failed on creating release."
   end
+end
+
+def set_output(name, value)
+  puts "::set-output name=#{name}::#{value}"
 end
 
 def release_homebrew(github_token, tag, root, package_file_path)
